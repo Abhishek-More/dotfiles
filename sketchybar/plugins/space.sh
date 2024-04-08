32fa682bb978f6336fb389878a5d71f22f61d0ae
Abhishek More <abhimore74@gmail.com>
2024-04-08 18:08:07 -0500

    pacman spacing

--- sketchybar/plugins/space.sh
+++ sketchybar/plugins/space.sh
@@ -9,12 +9,15 @@ source "$CONFIG_DIR/colors.sh" # Loads all defined colors
 if [ $SELECTED = true ]; then
 	sketchybar --set $NAME background.drawing=off \
 		icon="󰊠" \
+		icon.width=12 \
 		icon.font="JetBrainsMono Nerd Font:Semibold:19.0" \
-		icon.padding_left=4 \
 		icon.color=$WHITE
 else
 	sketchybar --set $NAME background.drawing=off \
 		icon="" \
+		icon.width=12 \
+		icon.position=center \
+		icon.padding_left=2 \
 		icon.font="JetBrainsMono Nerd Font:Semibold:12.0" \
 		icon.color=$WHITE
 fi
