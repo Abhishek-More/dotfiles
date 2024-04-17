242b306a42947c4a80ca79b975a034c68634c84b
Abhishek More <abhimore74@gmail.com>
2024-04-17 14:03:27 -0500

    w alias

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+-- I keep accidentally running :W instead of :w
+vim.cmd("command! -nargs=0 W w")
+
 -- require("flit").setup({
 --   keys = { f = "f", F = "F", t = "t", T = "T" },
 --   -- A string like "nv", "nvo", "o", etc.
